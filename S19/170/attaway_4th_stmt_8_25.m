bfc28d3810af66dbae9e8e1d84fc29a9dfa45e1b
Dr. Douglas De Boer, P.E., GitHub noob <Douglas.DeBoer@Dordt.edu>
2019-02-09 18:31:45 -0600

    Add files via upload

---
+++ S19/170/attaway_4th_stmt_8_25.m
@@ -0,0 +1,16 @@
+% A script to enter the structure given in Attaway 4th ed, Problem 8.25
+%
+% If you have trouble running this script try restarting Matlab or Octave 
+% or try the clear command to erase all your variables, then try this 
+% script again.
+
+kit(2).sub.id = 123;
+kit(2).sub.wt = 4.4;
+kit(2).sub.code = 'a';
+kit(2).name = 'xyz';
+kit(2).lens = [4 7];
+kit(1).name = 'rst';
+kit(1).lens = 5:6;
+kit(1).sub.id = 33;
+kit(1).sub.wt = 11.11;
+kit(1).sub.code = 'q'; 
