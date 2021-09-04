2e4b4804c0aeabf00311a0b0282e545946c47696
dfdeboer <douglas.deboer@dordt.edu>
2021-09-03 19:49:20 -0500

    Updated homepage to show "emeritus" Also updated HoW_S with some shotgun mic info.

--- HOW_S/MICS/mic_polar.m
+++ HOW_S/MICS/mic_polar.m
@@ -5,10 +5,11 @@ theta = pi*[-180:1:180]/180;  % degrees converted to radians
 
 % patterns, uncomment one
 %gain = abs(1 +     0*theta); %Omni
-gain = abs(0.9 +     0.1*cos(theta)); %Omni practical
+%gain = abs(0.9 +     0.1*cos(theta)); %Omni practical
 %gain = abs(0.5 +   0.5*cos(theta)); %Cardioid 
 %gain = abs(0.33 + 0.67*cos(theta)); %Super Cardioid 
 %gain = abs(0.0 +   1.0*cos(theta)); %Figure 8 
+gain = abs((cos(4*theta).*(0.33 + 0.67*cos(theta).^5))); %Figure 8 
 clf;
 polar(theta, gain, '-g');
 set (gca, "fontsize", 20, "linewidth", 2, "rtick", 0.2:0.2:1, "ttick", 0:30:330);
